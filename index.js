fc24128d5f69c32ce8a8e7a25c2656f10f205e64
terribledevice <longlost@gmail.com>
2020-10-13 23:35:40 -0400

    finished most of step 3, just need to render movies to screen

--- index.js
+++ index.js
@@ -2,24 +2,25 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     renderMovies = (movieArray) => {
-            let movieHTML = movieArray.map((currentMovie) => {
-                const title = currentMovie.Title;
-                console.log(title);
-                const year = currentMovie.Year;
-                console.log(year);
-                const imdbID = currentMovie.imdbID;
-                console.log(imdbID);
-                const type = currentMovie.Type;
-                console.log(type);
-                const poster = currentMovie.Poster;
-                console.log(poster);
+        let movieHTML = movieArray.map((currentMovie) => {
+            const title = currentMovie.Title;
+            console.log(title);
+            const year = currentMovie.Year;
+            console.log(year);
+            const imdbID = currentMovie.imdbID;
+            console.log(imdbID);
+            const type = currentMovie.Type;
+            console.log(type);
+            const poster = currentMovie.Poster;
+            console.log(poster);
 
 
-            })
-            return movieHTML.join('');
-        }
-        // document.body.innerHTML = renderMovies(movieData);
-    document.getElementById('search-form').addEventListener(‘submit’, (e) => {
+        })
+        return movieHTML.join('');
+    }
+
+    document.getElementById('search-form').addEventListener("submit", (e) => {
+        e.preventDefault();
         return renderMovies(movieData);
     })
 
