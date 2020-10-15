a90e327b7803a02ba34dcad1a4337b254025d91e
terribledevice <longlost@gmail.com>
2020-10-14 20:17:00 -0400

    Now the movies render to the screen after hitting 'submit'

--- index.js
+++ index.js
@@ -10,20 +10,25 @@ function renderMovies() {
         text += `
             <div class="movie card">
             <img class="card-img-top" src="${movie.Poster}" alt="${movie.Title}">
-            <div class="card-img-overlay">
+            <div class="card-img-bottom">
                 <h4 class="card-title">${movie.Title}</h4>
                 <p class="card-text">${movie.Year}</p>
-                <a href="#" class="btn btn-primary">Add</a>
+                <a href="#" class="btn btn-info stretched-link">Add</a>
             </div>
         </div>`;
 
     });
 
-    document.getElementById("movie-cards").innerHTML = text;
+    // 
+    document.getElementById("search-form").addEventListener("submit", function(e) {
+        e.preventDefault();
+        document.getElementById("movie-cards").innerHTML = text;
+    })
+
 }
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    renderMovies();
 
+    renderMovies();
 });
