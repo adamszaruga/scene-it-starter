504bda67f0b642c165ca05ee3d65bda1b891a775
jnutterdev <hello@jnutterdev.com>
2021-01-12 17:20:10 -0500

    Able to search for movies either via API or cache, and display them on the page. Just need to be able to save a movie to the watchlist now

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,16 @@
 const axios = require("axios");
 const apikey = process.env.APIKEY;
+const mongoose = require('mongoose');
+// const redis = require("../utils/redis");
+const redis = require('redis');
+const Watchlist = mongoose.model('Watchlist');
+
+const redisPort = 6379;
+const client = redis.createClient(redisPort);
+
+client.on("error", (err) => {
+    console.log(err);
+})
 
 // Form method GET uses req.query
 // Form method POST uses req.body
@@ -11,34 +22,35 @@ exports.mainHome = (req, res) => {
 
 // Main movie serch controller
 exports.movieSearch = async(req, res) => {
-
+    
     try {
+        // saves GET results to watchlist object
+        const searchDetails = req.query.title;
+  
+        client.get(searchDetails, async (err, title) => {
+            if (err) throw err;
 
-        const moviename = req.query.title.split(' ').join('+');
+            if (title) {
+                
+                const cachedData = JSON.parse(title);
+                res.render('./partials/content', { movieData: cachedData.Search });
+                res.end();
+            } else {
+                const moviename = searchDetails.split(' ').join('+');
+                const options = {
+                    method: "get",
+                    url: `http://www.omdbapi.com/?apikey=${apikey}&s=${moviename}`
         
-        const options = {
-            method: "get",
-            url: `http://www.omdbapi.com/?apikey=${apikey}&s=${moviename}`
-
-        };
-
-        const movieAPI = await axios.request(options);
-        const moviesData = movieAPI.data;
-
-        res.render('./partials/content', { movieData: moviesData.Search });
-       
-
-    } catch (err) {
-        if (err.response) {
-            console.log(err.response.data);
-        } else if (err.request) {
-            console.log(err.request);
-        } else {
-            console.error("Error", err.message);
+                };
+                const movieAPI = await axios.request(options);
+                client.setex(searchDetails, 600, JSON.stringify(movieAPI.data));
+                res.render('./partials/content', { movieData: movieAPI.data.Search });
+                res.end();
+            }
         }
+        ) 
+    }   catch(err) {
+        res.status(500).send({message: err.message});
     }
-
-    res.end();
-
-};
+}
 
--- controllers/watchlist.controllers.js
+++ controllers/watchlist.controllers.js
@@ -1,14 +1,22 @@
 const axios = require("axios");
 const apikey = process.env.APIKEY;
 const mongoose = require('mongoose');
+const redis = require('redis');
 const Watchlist = mongoose.model('Watchlist');
 
+const redisPort = 6379;
+const client = redis.createClient(redisPort);
+
+client.on("error", (err) => {
+    console.log(err);
+})
 
 
 // GET main list of movies saved in watchlist
 exports.getWatchlist = async (req, res) => {
     
     try {
+        
         const movies = await Watchlist.find().cache({ expire: 10});
         res.render('./partials/content', { movieData: movies});
     } catch (err) {
@@ -25,27 +33,30 @@ exports.getMovieFromWatchlist = async(req, res) => {
 // POST a new movie to the watchlist
 
 exports.addMovieToWatchList = async (req, res) => {
-    const { Title, Genre, Rated, Released, Runtime, Plot, Poster, imdbID } = req.body;
-
-    if ( !Title || !Poster || !imdbID ) {
-        return res.status(400).send('Missing Title, Poster, or imdbID');
-    }
-
-            const movie = new Watchlist({
-        Title, 
-        Genre, 
-        Rated, 
-        Released, 
-        Runtime, 
-        Plot, 
-        Poster, 
-        imdbID
-    });
+    const { id } = req.params;
+    console.log(typeof id);
 
     try {
-        await movie.save();
-        res.send(movie);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+    client.get(id, async (err, title) => {
+        if (err) throw err;
+        if (title) {
+            const cachedData = JSON.parse(title);
+            console.log(title);
+            // res.render('./partials/content', { movieData: cachedData.Search });
+            res.end();
+        }
+    })
+} catch (err) {
+        res.status(500).send({message: err.message});
+    
+}
+
+    // try {
+    //     await Watchlist.find().cache({ expire: 86400})
+    //     .then(res => { movie.save() })
+    //     .then(item => {res.send(movie)})
+        
+    // } catch (err) {
+    //     res.status(400).send(err.message);
+    // }
 };
