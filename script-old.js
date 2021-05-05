173f9b69511f05f372fd48aaf354b9422b239ce3
Heggy Castaneda <heggyy@gmail.com>
2021-05-05 01:59:22 -0400

    finish localStorage

--- script-old.js
+++ script-old.js
@@ -34,7 +34,7 @@ let renderMovies = movies => {
           <div class="card-body">
             <span class="badge badge-secondary">${movie.Year}</span>
             <h5 class="card-title">${movie.Title}</h5>
-            <button class="btn btn-primary add-movie" onclick="saveToWatchlist(${movie.imdbID.toString()})" >Add</a>
+            <button class="btn btn-primary add-movie" onclick="saveToWatchlist('${movie.imdbID}')" >Add</a>
           </div>
         </div>
       </div>
@@ -56,7 +56,32 @@ if no img =>
  * 
  */
 
+// 1. saveToWatchlist has a parameter called **imdbID** which will tell us which movie the user clicked on. We’ll use it to sift through movieData.js to find the relevant movie information. variable movie has the movie information that we want in our watchlist, 
+const saveToWatchlist = imdbID => {
+  // console.log("clicked movie imdbID: ", imdbID);
+  let movie = movieData.find(currentMovie => {
+    console.log("clicked movie imdbID: ", currentMovie);
+    console.log("currentMovie.imdbID: ", currentMovie.imdbID);
+    console.log("imdbID: ", imdbID)
+    return currentMovie.imdbID === imdbID;
+  });
+    // pull in the watchlist from localStorage, add it to the watchlist, and then save the watchlist back to localStorage
 
-let saveToWatchlist = imdbID => {
-  console.log("clicked movie imdbID: ", imdbID);
+  let watchlistJSON;
+  let watchlist;
+  console.log("movie: ?", movie);
+  if(localStorage.getItem('watchlist') === null){
+    watchlist = [];
+    watchlist.push(movie);
+    watchlistJSON = JSON.stringify(watchlist);
+    localStorage.setItem('watchlist', watchlistJSON);
+  } else {
+    watchlistJSON = localStorage.getItem('watchlist');
+    watchlist = JSON.parse(watchlistJSON);
+    watchlist.push(movie);
+    watchlistJSON = JSON.stringify(watchlist);
+    localStorage.setItem('watchlist', watchlistJSON);
+  }
 }
+
+
